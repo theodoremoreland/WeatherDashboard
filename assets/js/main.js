dbbe9dc444570673db670830834d9874d304d87e
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-17 13:46:16 -0500

    added displayCurrentWeather function + linked with submit event so now displays current weather

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,24 @@
 import { extractWeatherData } from "./scripts/fetchWeatherData.js";
+import { displayCurrentWeather } from "./scripts/displayCurrentWeather.js";
 
 const formElement = document.querySelector("form");
+const currentWeatherContainer = document.querySelector(".currentWeather");
 
 const submitSearch = async (event) => {
     event.preventDefault();
 
+    let weatherData;
+    
     const searchValue = document.querySelector("input").value;
     const [city, stateCode, countryCode] = searchValue.split(",");
 
     await extractWeatherData(city, stateCode.trim(), countryCode.trim())
-        .then(data => console.log(data))
+        .then(data => {
+            displayCurrentWeather(currentWeatherContainer, data[0]);
+        })
         .catch(e => console.error(e))
         ;
+    
 }
 
 formElement.addEventListener("submit", submitSearch);
--- assets/js/scripts/displayCurrentWeather.js
+++ assets/js/scripts/displayCurrentWeather.js
@@ -1,7 +1,52 @@
-// City
-// Date
-// Icon (indicates type of weather)
-// Temp
-// Wind
-// Humidity
-// UV Index
+/**
+ * Adds currentWeather elements and data to DOM
+ * @param {Node} currentWeatherContainer - DOM node to add weather data and elements.
+ * @param {Object} data - Data object, including name, date, uvi, temp, windSpeed, humidity and weatherIcon.
+ */
+export const displayCurrentWeather = (currentWeatherContainer, { name, date, uvi, temp, windSpeed, humidity, weatherIcon }) => {
+    currentWeatherContainer.innerHTML = "";
+
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    const div3 = document.createElement("div");
+
+    const ul = document.createElement("ul");
+    const li1 = document.createElement("li");
+    const li2 = document.createElement("li");
+    const li3 = document.createElement("li");
+
+    const cityHeader = document.createElement("h2");
+    const dateParagraph = document.createElement("p");
+    const uvLabelParagraph = document.createElement("p");
+    const uvValueParagraph = document.createElement("p");
+
+    cityHeader.textContent = `${name}`;
+    dateParagraph.textContent = `${date}`;
+    cityHeader.setAttribute("class", "city");
+    dateParagraph.setAttribute("class", "date");
+
+    div1.appendChild(cityHeader);
+    div1.appendChild(dateParagraph);
+
+    uvLabelParagraph.textContent = `UV Index`;
+    uvValueParagraph.textContent = `${uvi}`;
+    uvLabelParagraph.setAttribute("class", "uvLabel");
+    uvValueParagraph.setAttribute("class", "uvValue");
+
+    div2.appendChild(uvLabelParagraph);
+    div2.appendChild(uvValueParagraph);
+
+    li1.textContent = `Temp: ${temp}`;
+    li2.textContent = `Humidity: ${humidity}%`;
+    li3.textContent = `Wind Speed: ${windSpeed}mph`;
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    div3.appendChild(ul);
+
+    currentWeatherContainer.appendChild(div1);
+    currentWeatherContainer.appendChild(div2);
+    currentWeatherContainer.appendChild(div3);
+};
