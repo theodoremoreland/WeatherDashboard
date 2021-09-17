892cd3bf85e615aac4bb8461d97deb2fe5ca8e30
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-17 13:07:05 -0500

    added submit search function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 import { extractWeatherData } from "./scripts/fetchWeatherData.js";
 
-extractWeatherData("Dallas", "TX", "USA")
-    .then(data => console.log(data));
+const formElement = document.querySelector("form");
+
+const submitSearch = async (event) => {
+    event.preventDefault();
+
+    const searchValue = document.querySelector("input").value;
+    const [city, stateCode, countryCode] = searchValue.split(",");
+
+    await extractWeatherData(city, stateCode.trim(), countryCode.trim())
+        .then(data => console.log(data))
+        .catch(e => console.error(e))
+        ;
+}
+
+formElement.addEventListener("submit", submitSearch);
