ef99b9ec30d4e0c2e7a6da98160127644e65fa69
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-18 01:13:34 -0500

    added feels like stat to current weather

--- assets/js/scripts/displayCurrentWeather.js
+++ assets/js/scripts/displayCurrentWeather.js
@@ -17,9 +17,9 @@ export const determineUVlevel = (index) => {
 /**
  * Adds currentWeather elements and data to DOM
  * @param {Node} currentWeatherContainer - DOM node to add weather data and elements.
- * @param {Object} data - Data object, including name, date, uvi, temp, windSpeed, humidity weatherIcon, and description.
+ * @param {Object} data - Data object, including name, date, uvi, temp, windSpeed, humidity, feelsLike, weatherIcon, and description.
  */
-export const displayCurrentWeather = (currentWeatherContainer, { name, date, uvi, temp, windSpeed, humidity, weatherIcon, description }) => {
+export const displayCurrentWeather = (currentWeatherContainer, { name, date, uvi, temp, windSpeed, humidity, feelsLike, weatherIcon, description }) => {
     currentWeatherContainer.innerHTML = "";
 
     const div1 = document.createElement("div");
@@ -30,6 +30,7 @@ export const displayCurrentWeather = (currentWeatherContainer, { name, date, uvi
     const li1 = document.createElement("li");
     const li2 = document.createElement("li");
     const li3 = document.createElement("li");
+    const li4 = document.createElement("li");
 
     const cityHeader = document.createElement("h2");
     const dateParagraph = document.createElement("p");
@@ -53,13 +54,15 @@ export const displayCurrentWeather = (currentWeatherContainer, { name, date, uvi
     div2.appendChild(uvValueParagraph);
     div2.setAttribute("class", determineUVlevel(uvi));
 
-    li1.textContent = `Temp: ${temp}`;
+    li1.textContent = `Temp: ${temp}d`;
     li2.textContent = `Humidity: ${humidity}%`;
     li3.textContent = `Wind Speed: ${windSpeed}mph`;
+    li4.textContent = `Feels Like: ${feelsLike}d`;
 
     ul.appendChild(li1);
     ul.appendChild(li2);
     ul.appendChild(li3);
+    ul.appendChild(li4);
 
     div3.appendChild(ul);
 
