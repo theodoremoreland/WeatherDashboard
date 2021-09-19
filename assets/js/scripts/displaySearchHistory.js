706b9139367a967991a4b5c5b728050f0e41d7fe
Theodore Moreland <theodoremoreland@yahoo.com>
2021-09-18 23:28:55 -0500

    no search history test displays if no history + added state and country to placeholder in input

--- assets/js/scripts/displaySearchHistory.js
+++ assets/js/scripts/displaySearchHistory.js
@@ -3,15 +3,17 @@
  * @param {Node} ulElement - Parent ul element to be appended to.
  */
 export const displaySearchHistory = (ulElement) => {
-    ulElement.innerHTML = "";
-
     const searchHistory = JSON.parse(localStorage.getItem("history")) || [];
 
-    for (const searchValue of searchHistory) {
-        const li = document.createElement("li");
-        li.classList.add("historicSearchValue");
-        li.textContent = searchValue;
-        li.dataset.searchValue = searchValue;
-        ulElement.appendChild(li);
+    if (searchHistory.length > 0) {
+        ulElement.innerHTML = "";
+
+        for (const searchValue of searchHistory) {
+            const li = document.createElement("li");
+            li.classList.add("historicSearchValue");
+            li.textContent = searchValue;
+            li.dataset.searchValue = searchValue;
+            ulElement.appendChild(li);
+        }
     }
 }
