0fa3f9f6e825c9009da6d55a702533bb09393809
Theodore Moreland <theodoremoreland@yahoo.com>
2021-10-28 16:02:31 -0500

    updated header and aside style plus history items are now clouds

--- assets/js/scripts/displaySearchHistory.js
+++ assets/js/scripts/displaySearchHistory.js
@@ -10,9 +10,16 @@ export const displaySearchHistory = (ulElement) => {
     if (searchHistory.length > 0) {
         for (const searchValue of searchHistory) {
             const li = document.createElement("li");
-            li.classList.add("historicSearchValue");
-            li.textContent = searchValue;
+            const img = document.createElement("img");
+            const p = document.createElement("p");
+
+            img.src = "assets/images/cloud.svg";
+            p.textContent = searchValue;
             li.dataset.searchValue = searchValue;
+            li.classList.add("historicSearchValue");
+
+            li.appendChild(p);
+            li.appendChild(img);
             ulElement.appendChild(li);
         }
     }
